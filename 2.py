eed3eaa3980ce7a8f2ebfa63cb8de934ee093875
RouzbehAB <roozbeh7abadi@gmail.com>
2024-11-18 20:09:21 +0330

    the beginning

--- 2.py
+++ 2.py
@@ -5,13 +5,24 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import seaborn as sns
 
-btc = yf.download("BTC-USD", start="2014-01-01", end="2024-01-01")
+btc = yf.download("BTC-USD", start="2015-01-01", end="2022-12-31")
 df = pd.DataFrame(btc)
 
 weekday = df.index.weekday
 df["weekday"] = weekday
-# print(weekday)
+
+
+sunday = df["weekday"] == 0
+wednesday = df["weekday"] == 3
+
+
+df_sunday = pd.merge(df, sunday, join="inner", on="Date")
+df_wednesday = pd.merge(df, wednesday, join="inner", on="Date")
+print(df)
+
+
+
 
 # print(df.describe())
-df.info()
-print(df.head())
+# df.info()
+# print(df.head())
